2b0d7a05e167535f23a665a220e31b21381de1b3
fishman <cy13692810010@163.com>
2024-05-14 20:56:01 +0800

    等一下看看线程池参数

--- security/src/main/java/com/fishman/security/SecurityApplication.java
+++ security/src/main/java/com/fishman/security/SecurityApplication.java
@@ -1,13 +1,31 @@
 package com.fishman.security;
 
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+        // 每333毫秒创建150线程，每个线程创建一个512kb的对象，最多一秒同时存在450线程，占用内存225m，查看6C的情况
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+            new Thread(() -> {
+                for (int i = 0; i < 150; i++) {
+                    try {
+                        //  不干活，专门创建512kb的小对象
+                        byte[] temp = new byte[1024 * 512];
+                        Thread.sleep(new Random().nextInt(100)); // 随机睡眠200毫秒秒以内
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }, 1000, 333, TimeUnit.MILLISECONDS);
+    }
 
 }
--- security/src/main/java/com/fishman/security/config/Jvmconfig.java
+++ security/src/main/java/com/fishman/security/config/Jvmconfig.java
@@ -1,9 +1,18 @@
 package com.fishman.security.config;
 
+import java.io.IOException;
+
 public class Jvmconfig {
     public static void setMemoryParams(String initialHeap, String maxHeap) {
         // 设置初始堆大小和最大堆大小
         System.setProperty("java.vm.args", initialHeap + " " + maxHeap);
+        String runCmd = String.format("java -Xmx256m -Dfile.encoding=UTF-8 -cp %s Main %s");
+        try {
+            Process runProcess = Runtime.getRuntime().exec(runCmd);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
